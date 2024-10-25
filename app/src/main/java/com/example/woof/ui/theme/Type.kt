c1673e2d82c9f04ac69e8e493c9eb8b1964195e7
Student <amelia.welhelmina@students.utdi.ac.id>
2024-10-25 12:10:36 +0700

    Woofmella

---
+++ app/src/main/java/com/example/woof/ui/theme/Type.kt
@@ -0,0 +1,7 @@
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.example.woof.R
+
+val AbrilFatface = FontFamily(
+    Font(R.font.abril_fatface_regular)
+)
